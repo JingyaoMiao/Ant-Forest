ece3777c2686f9edff8141222493c0cbff10bb29
Kyungun Bae <49053063+skf1396@users.noreply.github.com>
2019-10-08 09:33:24 +1300

    Add files via upload

--- App.js
+++ App.js
@@ -1,69 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, StatusBar, Image, ImageBackground,
+  TextInput, Alert, TouchableOpacity, Dimensions} from 'react-native';
+//import {imagebutton} from 'react-native-image-button-text'
+import MenuItem from './components/MenuItem';
+import Header from './components/Header';
 
-import { AppLoading, Asset } from 'expo';
+const {height, width} = Dimensions.get("window");
 
-import Navigation from './navigation';
-import { Block } from './components';
-
-// import all used images
-const images = [
-  require('./assets/icons/back.png'),
-  require('./assets/icons/plants.png'),
-  require('./assets/icons/seeds.png'),
-  require('./assets/icons/flowers.png'),
-  require('./assets/icons/sprayers.png'),
-  require('./assets/icons/pots.png'),
-  require('./assets/icons/fertilizers.png'),
-  require('./assets/images/plants_1.png'),
-  require('./assets/images/plants_2.png'),
-  require('./assets/images/plants_3.png'),
-  require('./assets/images/explore_1.png'),
-  require('./assets/images/explore_2.png'),
-  require('./assets/images/explore_3.png'),
-  require('./assets/images/explore_4.png'),
-  require('./assets/images/explore_5.png'),
-  require('./assets/images/explore_6.png'),
-  require('./assets/images/illustration_1.png'),
-  require('./assets/images/illustration_2.png'),
-  require('./assets/images/illustration_3.png'),
-  require('./assets/images/avatar.png'),
-];
-
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  }
-
-  handleResourcesAsync = async () => {
-    // we're caching all the images
-    // for better performance on the app
-
-    const cacheImages = images.map(image => {
-      return Asset.fromModule(image).downloadAsync();
-    });
-
-    return Promise.all(cacheImages);
-  }
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this.handleResourcesAsync}
-          onError={error => console.warn(error)}
-          onFinish={() => this.setState({ isLoadingComplete: true })}
-        />
-      )
-    }
-
-    return (
-      <Block white>
-        <Navigation />
-      </Block>
-    );
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Header title="Plant a Tree"/>
+        <View style={styles.column}>
+        <Text style={styles.columnText}>Log in</Text>
+        </View>
+        <View style={styles.menuContainer}>
+          <MenuItem itemImage={require('./assets/news.png')}/>
+          <MenuItem itemImage={require('./assets/Upload.png')}/>
+          <MenuItem itemImage={require('./assets/FruitTree.png')}/>
+          <MenuItem itemImage={require('./assets/HedgeTree.png')}/>
+          <MenuItem itemImage={require('./assets/EvergreenTree.png')}/>
+          <MenuItem itemImage={require('./assets/NZNativeTree.png')}/>
+          <MenuItem itemImage={require('./assets/GumTree.png')}/>
+          <MenuItem itemImage={require('./assets/PalmTree.png')}/>
+          <MenuItem itemImage={require('./assets/HardwoodTree.png')}/>
+          </View>
+          
+      </View> 
+    );  
   }
 }
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+  column:{
+    width:'100%',
+    height:70,
+    paddingTop:10,
+    backgroundColor:'grey',
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  columnText:{
+      color:'black',
+      fontSize:25,
+      fontWeight:'700',
+  },
+  MenuItem:{
+    width: '50%',
+    height: '50%',
+    padding:20,
+    backgroundColor:'green',
+    borderColor:'black',
+    borderWidth:2,
+  },
+  image:{
+    width:'100%',
+    height:'100%',
+    opacity:0.8,
+    borderColor:'black',
+    borderWidth:3,
+  },
+
+  menuContainer:{
+    height: '40%',
+    flexDirection:'row',
+    flexWrap:'wrap',
+  },
+  overlayContainer:{
+    flex:1,
+    backgroundColor:'black',
+
+  }
 });
